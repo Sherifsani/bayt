5dc50ea3f337e6640a2d8d95c5206fec66c28292
sherifsani <sanisherif11@gmail.com>
2025-01-22 15:34:15 +0100

    .

--- app/_components/HeroProjects.tsx
+++ app/_components/HeroProjects.tsx
@@ -5,7 +5,7 @@ const HeroProjects = () => {
   return (
     <section className="hero-projects mt-10 flex flex-col gap-4">
       <h2 className="font-[500]">What I've been up to...</h2>
-      <div className="project-grid grid gap-5">
+      <div className="project-grid grid gap-10">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
--- app/_components/micros/ProjectCard.tsx
+++ app/_components/micros/ProjectCard.tsx
@@ -3,19 +3,31 @@ import project1 from "../../../public/project-1.png";
 
 const ProjectCard = () => {
   return (
-    <div className="project-card flex flex-col gap-3">
-      <Image src={project1} alt="project1" className="w-3/4 p-2 bg-[#eceaea] rounded-md" />
+    <div className="project-card flex flex-col gap-3 shadow-sm py-3 rounded-sm">
+      <Image
+        src={project1}
+        alt="project1"
+        className="rounded-md w-1/2 h-auto"
+      />
       <h3 className="">Wordpress App deployment</h3>
       <p className="text-[15px] text-gray-600">
         Deployed a wordpress image on AWS ECS using best practices and ensured
         high availability all managed through terraform
-          </p>
-          <div className="flex gap-2">
-              <span className="border rounded-full p-1 text-sm border-[#eceaea]">aws</span>
-              <span className="border rounded-full p-1 text-sm border-[#eceaea]">terraform</span>
-              <span className="border rounded-full p-1 text-sm border-[#eceaea]">ecs</span>
-              <span className="border rounded-full p-1 text-sm border-[#eceaea]">rds</span>
-          </div>
+      </p>
+      <div className="flex gap-2">
+        <span className="text-[#565454] p-[.2rem] bg-[#eceaea] rounded-full text-sm">
+          aws
+        </span>
+        <span className="border rounded-full p-1 px-3 text-sm border-[#eceaea]">
+          terraform
+        </span>
+        <span className="border rounded-full p-1 px-3 text-sm border-[#eceaea]">
+          ecs
+        </span>
+        <span className="border rounded-full p-1 px-3 text-sm border-[#eceaea]">
+          rds
+        </span>
+      </div>
     </div>
   );
 };
